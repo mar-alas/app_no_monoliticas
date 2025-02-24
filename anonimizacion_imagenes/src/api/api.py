e7c319f56023ebc281aa7e38b7d1418c7255c4b7
Jhon Freddy Puentes <j.puentesn@uniandes.edu.co>
2025-02-24 15:42:43 -0500

    add: Implementacion reglas de negocio centralizadas y control de errores. (#2)
    Co-authored-by: Jhon Freddy Puentes Nuñez <jhonfredypuentesnunez@192.168.10.9>
    Co-authored-by: Daniel Alfonso Gamez Salcedo <da.gamez96@uniandes.edu.co>

--- anonimizacion_imagenes/src/api/api.py
+++ anonimizacion_imagenes/src/api/api.py
@@ -4,6 +4,7 @@ from io import BytesIO
 from flask import Flask, jsonify, request, send_file
 from src.aplicacion.servicio_anonimizar import servicio_anonimizar_imagen
 from src.infraestructura.publicadores import PublicadorEventos
+from src.seedwork.dominio.reglas import FormatoDeImagenEsValido, NombreDeImagenNoPuedeSerVacio, ImagenDeAnonimizacionEsValida, TamanioDeImagenEsValido
 
 app = Flask(__name__)
 
@@ -13,38 +14,53 @@ def home():
 
 @app.route('/anonimizar-imagen', methods=['POST'])
 def anonimizar_imagen():
-    if 'image' not in request.files:
-        return jsonify(error="No image part"), 400
+    try:
+        if not ImagenDeAnonimizacionEsValida(request.files['image']).es_valido():
+            return jsonify(error="La imagen de anonimizacion no es valida"), 400
 
-    file = request.files['image']
-    
-    if file.filename == '':
-        return jsonify(error="No selected file"), 400
-
-    if file and file.filename.endswith('.jpeg'):
+        file = request.files['image']
+        
         image_data = file.read()
+
+        if not TamanioDeImagenEsValido(len(image_data)).es_valido():
+            return jsonify(error="El tamaño de la imagen no es valido"), 400
+
+        if not NombreDeImagenNoPuedeSerVacio(file.filename).es_valido():
+            return jsonify(error="El nombre de la imagen no puede ser vacio"), 400
+        
+        if not FormatoDeImagenEsValido(file.filename).es_valido():
+            return jsonify(error="Invalid file type, only .jpg, .png, jpeg allowed"), 400
+
+
         image_stream = BytesIO(image_data)
-        image_stream2=servicio_anonimizar_imagen(image_stream)
+        image_stream2 = servicio_anonimizar_imagen(image_stream)
+    
         # llamar a publicadores para publicar evento
-        publicador = PublicadorEventos('pulsar://localhost:6650')
-        evento = {
-            'evento': 'Imagen anonimizada',
-            'filename': file.filename,
-            'size': len(image_data)
-        }
-        publicador.publicar_evento(
-            'eventos-anonimizador',
-            str(evento)
-        )
-        publicador.cerrar()
+        try:
+            publicador = PublicadorEventos('pulsar://localhost:6650')
+            evento = {
+                'evento': 'Imagen anonimizada',
+                'filename': file.filename,
+                'size': len(image_data)
+            }
+            publicador.publicar_evento(
+                'eventos-anonimizador',
+                str(evento)
+            )
+            publicador.cerrar()
+        except Exception as e:
+            # Log the error but continue with the response
+            print(f"Error al publicar evento: {str(e)}")
+        
         return send_file(
             image_stream2,
             mimetype="image/jpeg",  # Correct MIME type for JPEG images
             as_attachment=False,    # Set to True if you want to force download
             download_name=file.filename  # Optional: Set the filename for the response
-        )
-    else:
-        return jsonify(error="Invalid file type, only .jpg allowed"), 400
+        ) 
+    except Exception as e:
+        return jsonify(error=f"Error inesperado: {str(e)}. Intente mas tarde.",), 500
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
