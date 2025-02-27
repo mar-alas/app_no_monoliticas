f2b1b86e19d3bdd3b49ae8ad3dce65a9097f2c00
Daniel Gamez <dgamez@porvenir.com.co>
2025-02-27 10:18:05 -0500

    actualizacion seedwork microservicio anonimizacion

---
+++ anonimizacion_imagenes/src/seedwork/aplicacion/dto.py
@@ -0,0 +1,17 @@
+#  Data Transfer Object (DTO) Clase base
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+@dataclass(frozen=True)
+class DTO():
+    ...
+
+class Mapeador(ABC):
+    @abstractmethod
+    def externo_a_dto(self, externo: any) -> DTO:
+        ...
+
+    @abstractmethod
+    def dto_a_externo(self, dto: DTO) -> any:
+        ...
+    
