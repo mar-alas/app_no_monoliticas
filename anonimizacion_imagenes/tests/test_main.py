3e09fe4cbd2441f5a98b6707d1be14b74f0c7610
Daniel Gamez <dgamez@porvenir.com.co>
2025-02-27 10:24:46 -0500

    fix: pruebas unitarias anonimizacion

--- anonimizacion_imagenes/tests/test_main.py
+++ anonimizacion_imagenes/tests/test_main.py
@@ -1,6 +1,17 @@
 import pytest
 from src.api.api import app
 from werkzeug.datastructures import FileStorage
+import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "secreto_super_seguro"
+
+def generate_token(user_id: str):
+        payload = {
+            "sub": str(user_id),
+            "exp": datetime.utcnow() + timedelta(days=1)
+        }
+        return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
 @pytest.fixture
 def client():
@@ -8,14 +19,18 @@ def client():
     with app.test_client() as client:
         yield client
 
+
+
 def test_home_route(client):
-    response = client.get('/')
+    token=generate_token("test_user")
+    response = client.get('/', headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == 200
     json_data = response.get_json()
     assert 'message' in json_data
     assert json_data['message'] == 'Welcome to the Flask app!'
 
 def test_anonimizar_route(client):
+    token=generate_token("test_user")
     # Prepare a test image file
     test_image_path = "tests/test_image.jpeg"
 
@@ -30,6 +45,7 @@ def test_anonimizar_route(client):
             "/anonimizar-imagen",
             data={"image": file_storage, "description": "Test image"},
             content_type="multipart/form-data",
+            headers={"Authorization": f"Bearer {token}"}
         )
 
     assert response.status_code == 200
