42e15b08364d4b55dc481cbc4536ece3f28463cc
Daniel Gamez <dgamez@porvenir.com.co>
2025-03-01 11:01:03 -0500

    se agregan comandos nuevos en infra de ingesta imagenes

--- ingesta_imagenes/src/infraestructura/schema/v1/comandos.py
+++ ingesta_imagenes/src/infraestructura/schema/v1/comandos.py
@@ -1,5 +1,7 @@
+import uuid
 from pulsar.schema import *
 from dataclasses import dataclass, field
+from src.seedwork.infraestructura.utils import time_millis
 from src.seedwork.infraestructura.schema.v1.comandos import (ComandoIntegracion)
 
 class IngestaImagenPayload(ComandoIntegracion):
@@ -13,3 +15,25 @@ class IngestaImagenPayload(ComandoIntegracion):
 
 class ComandoIngestaImagen(ComandoIntegracion):
     data = IngestaImagenPayload()
+
+class AnonimizarImagen(Record):
+    id_usuario = String()
+    fecha_creacion= Long()
+    id = String()
+    filename = String()
+    size = String()
+    binario = String()
+    mimetype = String()
+
+class ComandoAnonimizarImagen(ComandoIntegracion):
+    id = String(default=str(uuid.uuid4()))
+    time = Long()
+    ingestion = Long(default=time_millis())
+    specversion = String(default="v1")
+    type = String(default="AnonimizarImagen")
+    datacontenttype = String()
+    service_name = String(default="anonimizacion_imagenes")
+    data = AnonimizarImagen
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
